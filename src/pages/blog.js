58797dd8cd4015c6079689f39bf38cc8eeb0e731
Chris Patti <feoh@feoh.org>
2021-11-26 17:21:44 -0500

    Add dynamic blog posts

---
+++ src/pages/blog.js
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
+const BlogPage = ({ data }) => {
+    return (
+        <Layout pageTitle="My Blog Posts">
+            <ul>
+                {
+                    // data.allFile.nodes.map(node => (
+                    //     <li key={node.name}>
+                    //         {node.name}
+                    //     </li>
+                    // ))
+                    data.allMdx.nodes.map(node =>
+                        <article key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </article>
+                    )
+                }
+            </ul>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+            nodes {
+                frontmatter {
+                    date(formatString: "MMMM D, YYYY")
+                    title
+                }
+                id
+                body
+            }
+        }
+    }
+`
+
+export default BlogPage
