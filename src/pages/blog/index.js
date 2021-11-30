6d0511f61a291518ae0a98ba06ff65f113bb43fc
Chris Patti <feoh@feoh.org>
2021-11-30 04:06:05 -0500

    Fix post filenames. One page per post.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,29 +1,22 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { Link, graphql } from 'gatsby'
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    // data.allFile.nodes.map(node => (
-                    //     <li key={node.name}>
-                    //         {node.name}
-                    //     </li>
-                    // ))
-                    data.allMdx.nodes.map(node =>
-                        <article key={node.id}>
-                            <h2>{node.frontmatter.title}</h2>
-                            <p>Posted: {node.frontmatter.date}</p>
-                            <MDXRenderer>
-                                {node.body}
-                            </MDXRenderer>
-                        </article>
-                    )
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>
+                            <Link to={`/blog/${node.slug}`}>
+                                {node.frontmatter.title}
+                            </Link>
+                            <p>Posted: {node.frontmatter.date}</p> 
+                        </h2>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
@@ -37,7 +30,7 @@ export const query = graphql`
                     title
                 }
                 id
-                body
+                slug
             }
         }
     }
