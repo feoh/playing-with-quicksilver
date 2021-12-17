f7c7e5d47810ccef69d5d9c11aaec93a073d798b
Chris Patti <feoh@feoh.org>
2021-12-17 01:09:54 -0500

    Change css layout to flex.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,22 +1,25 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import { Link, graphql } from 'gatsby'
+import '../../misc-styles.css'
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            {
-                data.allMdx.nodes.map(node => (
-                    <article key={node.id}>
-                        <h2>
-                            <Link to={`/blog/${node.slug}`}>
-                                {node.frontmatter.title}
-                            </Link>
-                            <p>Posted: {node.frontmatter.date}</p> 
-                        </h2>
-                    </article>
-                ))
-            }
+            <div className='post-list'>
+                {
+                    data.allMdx.nodes.map(node => (
+                        <article key={node.id}>
+                            <h2>
+                                <Link to={`/blog/${node.slug}`}>
+                                    {node.frontmatter.title}
+                                </Link>
+                            </h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                        </article>
+                    ))
+                }
+            </div>
         </Layout>
     )
 }
