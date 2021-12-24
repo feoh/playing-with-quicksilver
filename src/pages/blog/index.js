2ca9df9d2dbed8d3719d9ca2912ee416396b543a
Chris Patti <feoh@feoh.org>
2021-12-24 16:50:52 -0500

    Remove styling that isn't working.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -6,20 +6,18 @@ import '../../misc-styles.css'
 const BlogPage = ({ data }) => {
     return (
         <Layout>
-            <div className='post-list'>
-                {
-                    data.allMdx.nodes.map(node => (
-                        <article key={node.id}>
-                            <h2>
-                                <Link to={`/blog/${node.slug}`}>
-                                    {node.frontmatter.title}
-                                </Link>
-                            </h2>
-                            <p>Posted: {node.frontmatter.date}</p>
-                        </article>
-                    ))
-                }
-            </div>
+            {
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>
+                            <Link to={`/blog/${node.slug}`}>
+                                {node.frontmatter.title}
+                            </Link>
+                        </h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
